cd0233d1553dc89dfd5ef303b2646825b02336a0
seanbaraka <seanbaraka@gmail.com>
2024-05-02 11:53:33 +0300

    update db query utils

--- accounts/src/lib/prisma.ts
+++ accounts/src/lib/prisma.ts
@@ -22,10 +22,31 @@ export async function createOrUpdateUser(user: User) {
     return { id: newUser.id };
   }
 }
+
 export async function getUser(userId: string) {
   return await prisma.user.findUnique({
     where: {
       id: userId,
+    },
+  });
+}
+
+export async function updateUserCredit(userId: string, amount: number) {
+  return await prisma.user.update({
+    where: {
+      id: userId,
+    },
+    data: {
+      creditsBalance: { increment: amount },
+    },
+  });
+}
+
+export async function createOrder(order: any) {
+  return await prisma.order.create({
+    data: {
+      userId: order.userId,
+      amount: order.amount,
     },
   });
 }
